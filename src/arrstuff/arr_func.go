cd11e82c3e5bc0dcb96120415f9e2414241d849d
bradleydamato <bradleydamato@gmail.com>
2020-04-19 08:53:48 -0400

    Figured out how arrays and slices work, different from python3!

--- src/arrstuff/arr_func.go
+++ src/arrstuff/arr_func.go
@@ -7,10 +7,23 @@ var q []int = []int{1,2,3}
 
 var r []int = []int{4,5}
 
+
+
 func main(){
+	//SHORTER WAY
+	x :=  append(q[:])
+	x =  append(x,r[:]...)
+
+	//LONG WAY
 	n := []int{}
 	for i := 0; i <= len(q)-1; i++ {
 		n = append(n,q[i])
 			}
+	for i := 0; i <= len(r)-1; i++ {
+		n = append(n,r[i])
+			}
 	fmt.Println(n)
+	fmt.Println(x)
 }
+
+
